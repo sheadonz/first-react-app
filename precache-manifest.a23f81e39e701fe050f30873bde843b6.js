4689178b071191bb6dad532e53cafe1463c88655
sheadonz <njxb.441@gmail.com>
2024-04-23 00:14:42 +0900

    Updates

---
+++ precache-manifest.a23f81e39e701fe050f30873bde843b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78e666d4858627b4b9228fe155cab360",
+    "url": "/first-react-app/index.html"
+  },
+  {
+    "revision": "bb4b7a2ee173c2222431",
+    "url": "/first-react-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8e48adee0e0bc49a6f36",
+    "url": "/first-react-app/static/js/2.19c7097c.chunk.js"
+  },
+  {
+    "revision": "bb4b7a2ee173c2222431",
+    "url": "/first-react-app/static/js/main.7a727093.chunk.js"
+  },
+  {
+    "revision": "cb2d0d1ef7d3e2b845c4",
+    "url": "/first-react-app/static/js/runtime~main.f5c4d8dc.js"
+  }
+]);
